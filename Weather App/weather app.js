f81e1c8110f35ffe315e0df9518ddfd84dd1dc1e
chase0793 <136058455+chase0793@users.noreply.github.com>
2023-08-29 13:02:54 +0200

    Add files via upload
    a mini-weather app

---
+++ Weather App/weather app.js
@@ -0,0 +1,32 @@
+let weather ={
+    "apiKey": "94dc8562c9fca8d26a1cae847e4dc270",
+    fetchWeather:function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey 
+        )
+        .then ((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+const{name} = data;
+const {icon, description} = data.weather[0];
+const{temp, humidty} = data.main;
+const{speed}= data.wind;
+console.log(name,icon,description,temp,humidty,speed)
+document.querySelector(".city").innerText = "Weather in" + name;
+document.querySelector(".description").innerText = description;
+document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+document.querySelector(".temp").innerText = temp + "C";
+document.querySelector(".humidity").innerText = "Humidity:" + humidty + "%";
+document.querySelector(".wind speed").innerText = "wind speed:" + speed + "km/ph";
+
+},
+search: function () {
+    // const city = document.querySelector(".search").value;
+    this.fetchWeather(document.querySelector(".search-bar").value)
+}
+};
+
+document.querySelector(".search button").addEventListener("click", function (){
+weather.search();
+})
+
